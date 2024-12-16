00d946bda89a4549df239c1e840a0f0030ac1778
Breslavets Maria <alwayslearn@mail.ru>
2024-12-16 19:37:32 +0300

    feat: solve the 10-th  problem (not to end)

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -174,8 +174,8 @@ function removeFirstOccurrences(str, value) {
  *   removeLastOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeLastOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeLastOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeLastOccurrences(str, value) {
+  return str.replace(value, '');
 }
 
 /**
