d60af1ca86126a8b4063e2fe08a838aa7817217b
Breslavets Maria <alwayslearn@mail.ru>
2024-12-23 14:05:54 +0300

    feat: solve the unbracketTag problem

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -416,8 +416,9 @@ function extractNameFromTemplate(value) {
  *   unbracketTag('<span>') => 'span'
  *   unbracketTag('<a>') => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  const one = str.replace('<', '');
+  return one.replace('>', '');
 }
 
 /**
